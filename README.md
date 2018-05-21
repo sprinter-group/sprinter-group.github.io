11624a8c9cf83488210662aaffb31417d45bfbea
spemer <ghsspower@naver.com>
2018-05-22 06:38:26 +0900

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Sprinter
+
+> The Sprinter is Todo list application.
+
+## Setup
+
+``` bash
+npm install
+gulp watch
+```
+
+## Repository
+
+- [Sprinter](https://github.com/sprinter-group)
+- [Sprinter - Web](https://github.com/sprinter-group/sprinter-web)
+- [Sprinter - Android](https://github.com/sprinter-group/sprinter-android)
+- [Sprinter - iOS](https://github.com/sprinter-group/sprinter-ios)
+- [Sprinter - Sketch](https://github.com/sprinter-group/sprinter-sketch)
+- [Blog](https://sprinter-group.github.io)
+
+## Design
+
+- [Behance](https://www.behance.net/gallery/54735807/Sprinter-To-do-list-task-management-application)
---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+"use strict";
+const gulp = require('gulp');
+
+const sass = require('gulp-sass'),
+      watch = require('gulp-watch'),
+      inject = require('gulp-inject'),
+      imagemin = require('gulp-imagemin'),
+      minifyjs = require('gulp-js-minify'),
+      cleanCSS = require('gulp-clean-css'),
+      autoprefixer = require('gulp-autoprefixer'),
+      browserSync = require('browser-sync').create();
+
+
+/* ==============================
+    static server
+============================== */
+gulp.task('browser-sync', function() {
+    browserSync.init(['*'], {
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('*').on('change', browserSync.reload);
+});
+
+
+/* ==============================
+    sass sync watch
+============================== */
+gulp.task('sass', function () {
+    return gulp.src('./css/src/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('./css/tmp'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./css/src/*.scss', ['sass']);
+});
+
+
+/* ==============================
+    css autoprefixer & minify -> tmp to dist
+============================== */
+gulp.task('prefix-css', function () {
+    return watch('./css/tmp/*.css', { ignoreInitial: false })
+        .pipe(autoprefixer())
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./css/dist'));
+});
+
+
+/* ==============================
+    minify js
+============================== */
+gulp.task('jsmin', function () {
+    return watch('./js/src/*.js', function () {
+        gulp.src('./js/src/*.js')
+            .pipe(minifyjs())
+            .pipe(gulp.dest('./js/dist'))
+            .pipe(browserSync.reload({stream: true}));
+    });
+});
+
+
+/* ==============================
+    image resizer
+============================== */
+gulp.task('img-resize', () =>
+	gulp.src('./images/src/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('./images/dist'))
+        .pipe(browserSync.reload({stream: true}))
+);
+
+
+/* ==============================
+    inject assets
+============================== */
+gulp.task('inject', function () {
+    let target = gulp.src('./index.html');
+    let sources = gulp.src(['./js/dist/*.js', './css/dist/style.css'], {read: false});
+
+    return target.pipe(inject(sources))
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+
+/* ==============================
+    gulp watch
+============================== */
+gulp.task('watch', ['browser-sync',
+                    'sass:watch',
+                    'prefix-css',
+                    'jsmin',
+                    'img-resize',
+                    'inject'])
